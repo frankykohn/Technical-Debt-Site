187aab6ac838e3748a7c077f0fcf9aa60e60598d
Frank <franky.kohn@gmail.com>
2021-12-01 23:15:14 -0800

    create terminal vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<template>
-  <iframe src='https://my.spline.design/glasscirclescopy-c8cdf0080f12846bde484ea6624a8690/' frameborder='0' width='100%' height='600px'></iframe>
+<template> 
+  <div>
+  </div>
 </template>
 
 <script>
